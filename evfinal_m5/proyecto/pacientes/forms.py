cfc742682b3ca15d7d7fda8c6d51dd6e3345e2fa
francisca <franciscacruzgantes@gmail.com>
2021-01-03 14:52:28 -0300

    agrego ultima version

---
+++ evfinal_m5/proyecto/pacientes/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+class Form_Usuario(forms.Form):
+    Nombre = forms.CharField()
+    Apellido= forms.CharField()
+    Edad= forms.IntegerField()
+    fecha_nacimiento = forms.DateField()
+
+class Form_Tratamiento(forms.Form):
+    fecha = forms.DateField()
+    diagnostico = forms.CharField()
+    tratamiento= forms.CharField()
+    control = forms.DateField()
+    comentarios= forms.CharField()
+
+class Form_Examen(forms.Form):
+    fecha = forms.DateField()
+    tipo = forms.CharField()
+    nombre= forms.CharField()
+    resultado= forms.CharField()
+    rango= forms.CharField()
+
+class Form_Login(forms.Form):   
+    usuario = forms.CharField()
+    contrasena= forms.CharField(widget=forms.PasswordInput)
+   
+    
